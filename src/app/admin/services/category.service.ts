691d34c3a96b9b8482965e40d98e1def58d03ffa
alavudheen.e <alavudheen.e@claysys.net>
2024-11-12 18:56:52 +0530

    category service modified

--- src/app/admin/services/category.service.ts
+++ src/app/admin/services/category.service.ts
@@ -14,27 +14,27 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   addCategory(data: any): Observable<Category> {
-    return this.http.post<Category>(`${this.baseUrl}/api/category'/category`, data);
+    return this.http.post<Category>(`${this.baseUrl}/api/category/category`, data);
   }
 
   categories(): Observable<Category[]> {
-    return this.http.get<Category[]>(`${this.baseUrl}/api/category'/categories`);
+    return this.http.get<Category[]>(`${this.baseUrl}/api/category/categories`);
   }
 
   updateCategory(id: number, category: Category): Observable<Category> {
-    return this.http.put<Category>(`${this.baseUrl}/api/category'/update/category/${id}`, category);
+    return this.http.put<Category>(`${this.baseUrl}/api/category/update/category/${id}`, category);
   }
 
   deleteCategory(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/api/category'/delete/category/${id}`);
+    return this.http.delete<void>(`${this.baseUrl}/api/category/delete/category/${id}`);
   }
 
   checkCategoryNameExists(name: string): Observable<boolean> {
-    return this.http.get<boolean>(`${this.baseUrl}/api/category'/exists/${name}`);
+    return this.http.get<boolean>(`${this.baseUrl}/api/category/exists/${name}`);
   }
 
 
   getCategoryById(id: number): Observable<Category> {
-    return this.http.get<Category>(`${this.baseUrl}/api/category'/categories/${id}`);
+    return this.http.get<Category>(`${this.baseUrl}/api/category/categories/${id}`);
   }
 }
